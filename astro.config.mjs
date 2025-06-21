ab1ffdd966ea2c8c23b833ef5ae4447798291f90
ydkmlt84 <justin@tacohouse.us>
2025-06-21 14:40:44 -0400

    New site created with Astro (#1)
    * initial commit on new page
    
    * uploading current
    
    * uploading current status
    
    * upload current progress
    
    * adding github pages action
    
    * removed base configuration
    
    * trying to fix images for github pages
    
    * fixed base to repo name and not branch name
    
    * fix favicon url
    
    * current status push
    
    * final_draft
    
    * fix image src
    
    * cleanup
    
    * pre-deployment commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+
+import icon from 'astro-icon'
+
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  site: 'https://ydkmlt84.github.io',
+  base: '/maintainerr_site/',
+})
